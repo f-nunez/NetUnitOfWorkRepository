f56aa96d61ae2eb2b90e6dfc22ecdd39b25b1f9f
Francisco Nuñez <fonfonferi@gmail.com>
2022-04-17 14:34:41 -0700

    Business layers added on dependendy injection container

--- Fnunez.UnitOfWork.Api/Program.cs
+++ Fnunez.UnitOfWork.Api/Program.cs
@@ -1,3 +1,4 @@
+using Fnunez.UnitOfWork.Business;
 using Fnunez.UnitOfWork.Data;
 using Fnunez.UnitOfWork.Data.Abstractions;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<UnitOfWorkDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient<CustomerBusiness, CustomerBusiness>();
+builder.Services.AddTransient<ShippingAddressBusiness, ShippingAddressBusiness>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
