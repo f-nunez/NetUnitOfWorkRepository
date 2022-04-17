ffdcb71dce6b1396925162363aa9c6356b23a33c
Francisco Nuñez <fonfonferi@gmail.com>
2022-04-16 22:09:37 -0700

    UnitOfWork added on dependency injection container

--- Fnunez.UnitOfWork.Api/Program.cs
+++ Fnunez.UnitOfWork.Api/Program.cs
@@ -1,10 +1,12 @@
 using Fnunez.UnitOfWork.Data;
+using Fnunez.UnitOfWork.Data.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<UnitOfWorkDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
