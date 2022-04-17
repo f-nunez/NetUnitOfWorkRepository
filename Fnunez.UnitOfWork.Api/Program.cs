6120ac663315cdc1671bb1d5ebac9aced87b3316
Francisco Nuñez <fonfonferi@gmail.com>
2022-04-16 22:08:47 -0700

    Connection string linked on Program class

--- Fnunez.UnitOfWork.Api/Program.cs
+++ Fnunez.UnitOfWork.Api/Program.cs
@@ -1,6 +1,10 @@
+using Fnunez.UnitOfWork.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UnitOfWorkDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
