3c6e2fb537f4eb5dabd0b4f4f6925667fe3c56ea
Francisco Nuñez <fonfonferi@gmail.com>
2022-04-17 14:30:57 -0700

    ShippingAddressBusiness CRUD example

--- Fnunez.UnitOfWork.Business/ShippingAddressBusiness.cs
+++ Fnunez.UnitOfWork.Business/ShippingAddressBusiness.cs
@@ -10,4 +10,41 @@ public class ShippingAddressBusiness : BusinessBase<ShippingAddressRepository, S
     public ShippingAddressBusiness(IUnitOfWork unitOfWork) : base(unitOfWork)
     {
     }
+
+    public async Task CreateByCustomers(List<Customer> customers)
+    {
+        List<ShippingAddress> shippingAddresses = new();
+        foreach (var customer in customers)
+        {
+            var shippingAddress = new ShippingAddress
+            {
+                Address = $"{customer.FirstName}'s address...",
+                CustomerId = customer.Id
+            };
+            shippingAddresses.Add(shippingAddress);
+        }
+
+        await InsertRangeAsync(shippingAddresses);
+    }
+
+    public async Task UpdateAll()
+    {
+        var shippingAddressesToUpdate = await GetAsync(x => x.IsActive);
+        if (shippingAddressesToUpdate is null || !shippingAddressesToUpdate.Any())
+            return;
+
+        for (int i = 0; i < shippingAddressesToUpdate.Count(); i++)
+            shippingAddressesToUpdate.ElementAt(i).Address = $"Address {i}";
+
+        await UpdateRangeAsync(shippingAddressesToUpdate);
+    }
+
+    public async Task DeleteAll()
+    {
+        var ShippingAddressesToDelete = await GetAsync(x => x.IsActive);
+        if (ShippingAddressesToDelete is null || !ShippingAddressesToDelete.Any())
+            return;
+        
+        await DeleteRangeAsync(ShippingAddressesToDelete);
+    }
 }
